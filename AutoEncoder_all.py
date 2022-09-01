3f770df727ef08e82c9157039482ac7598aa34c7
Channy <liqingchdu@163.com>
2022-09-01 10:02:39 +0800

    Add files via upload

--- AutoEncoder_all.py
+++ AutoEncoder_all.py
@@ -22,82 +22,86 @@ parser = argparse.ArgumentParser(description='Train Convolutionary Prototype Lea
 
 parser.add_argument('--epochs', default=300, type=int, help='total number of epochs to run')
 
-parser.add_argument('--data_name', default='mnist', type=str, help='dataset name to use')
-parser.add_argument('--data_channel', default=1, type=int, help='channel of dataset')
-parser.add_argument('--num_classes', default=10, type=int, help='class number for the dataset')
-parser.add_argument('--latent_dim', default=20, type=int, help='latent dimension of prototype feature')
-parser.add_argument('--K', default=10, type=int, help='sparse dimension of feature')
-
-# parser.add_argument('--data_name', default='fashion_mnist', type=str, help='dataset name to use')
+# parser.add_argument('--data_name', default='mnist', type=str, help='dataset name to use')
 # parser.add_argument('--data_channel', default=1, type=int, help='channel of dataset')
 # parser.add_argument('--num_classes', default=10, type=int, help='class number for the dataset')
 # parser.add_argument('--latent_dim', default=20, type=int, help='latent dimension of prototype feature')
 # parser.add_argument('--K', default=10, type=int, help='sparse dimension of feature')
 
-# parser.add_argument('--data_name', default='cifar10', type=str, help='dataset name to use')
-# parser.add_argument('--data_channel', default=3, type=int, help='channel of dataset')
+# parser.add_argument('--data_name', default='fashion_mnist', type=str, help='dataset name to use')
+# parser.add_argument('--data_channel', default=1, type=int, help='channel of dataset')
 # parser.add_argument('--num_classes', default=10, type=int, help='class number for the dataset')
-# parser.add_argument('--latent_dim', default=50, type=int, help='latent dimension of prototype feature')
-# parser.add_argument('--K', default=25, type=int, help='sparse dimension of feature')
 
-# parser.add_argument('--data_name', default='cifar100', type=str, help='dataset name to use')
+parser.add_argument('--data_name', default='cifar10', type=str, help='dataset name to use')
+parser.add_argument('--data_channel', default=3, type=int, help='channel of dataset')
+parser.add_argument('--num_classes', default=10, type=int, help='class number for the dataset')
+parser.add_argument('--latent_dim', default=20, type=int, help='latent dimension of autoencoder feature')
+parser.add_argument('--K', default=10, type=int, help='sparse dimension of feature')
+
+# parser.add_argument('--data_name', default='cifar100', type=str, help='dataset name to use')c
 # parser.add_argument('--data_channel', default=3, type=int, help='channel of dataset')
 # parser.add_argument('--num_classes', default=100, type=int, help='class number for the dataset')
-# parser.add_argument('--latent_dim', default=100, type=int, help='latent dimension of prototype feature')
-# parser.add_argument('--K', default=50, type=int, help='sparse dimension of feature')
 
 parser.add_argument('--feature_dim', default=512, type=int, help='feature dimension of original data')
-# parser.add_argument('--latent_dim', default=10, type=int, help='latent dimension of prototype feature')
-# parser.add_argument('--K', default=5, type=int, help='sparse dimension of feature')
-parser.add_argument('--batch_size', default=64, type=int, help='train batch size')
+parser.add_argument('--batch_size', default=64, type=int, help='train batch size') # 128 64
 parser.add_argument('--temp', default=0.1, type=float, help='trianing time temperature')
 parser.add_argument('--cls_weight', type=float, default=1e-1, help='ce learning weight')
 parser.add_argument('--pl_weight', type=float, default=1e-3, help='pl learning weight')
 parser.add_argument('--kl_weight', type=float, default=1e-1, help='kl divergence weight') # kl_weight
 parser.add_argument('--mse_weight', type=float, default=1e-2, help='mse learning weight')
-parser.add_argument('--use_sparse', type=Boolean, default=True, help='sparse autoencoder')
-# parser.add_argument('--use_sparse', type=Boolean, default=False, help='sparse autoencoder')
-
-parser.add_argument('--lr', type=float, default=1e-1, help='initial learning rate')
-parser.add_argument('--weight_decay', type=float, default=1e-10, help='weight decay')
+parser.add_argument('--lr', type=float, default=0.1, help='initial learning rate')
+parser.add_argument('--weight_decay', type=float, default=1e-10, help='weight decay') 
+parser.add_argument('--gamma', type=float, default=0.1, help='optimization gamma') 
+# parser.add_argument('--use_sparse', type=Boolean, default=True, help='sparse autoencoder')
+parser.add_argument('--use_sparse', type=Boolean, default=False, help='sparse autoencoder')
+# parser.add_argument('--l1', type=Boolean, default=True, help='L1')
+parser.add_argument('--l1', type=Boolean, default=False, help='L2')
 
 args = parser.parse_args()
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-
-def main(): # numclass=0
-    print(args)
-    if args.data_name=='mnist':
+cifar_train_transform = transforms.Compose([
+        transforms.ColorJitter(brightness=0.24705882352941178), # 随机改变图像的亮度对比度和饱和度
+        transforms.RandomRotation(degrees=5),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomCrop((32, 32), padding=3),
+        transforms.ToTensor(),
+    ])
+if args.data_name=='mnist':
         train_loader, val_loader = mnist_data_loader(batch_size=args.batch_size)
         test_set = MNIST(root='./data/mnist_data', train=False, transform=transforms.ToTensor(), download=True)
         image_shape = (1,1,28,28)
-    elif args.data_name=='fashion_mnist':
+elif args.data_name=='fashion_mnist':
         train_loader, val_loader = fashion_mnist_data_loader(batch_size=args.batch_size)
-    elif args.data_name=='cifar10':
+elif args.data_name=='cifar10':
         train_loader, val_loader = cifar10_data_loader(batch_size=args.batch_size)
-        CIFAR10_transform = transforms.Compose([
-            transforms.ColorJitter(brightness=0.24705882352941178), 
-            transforms.RandomRotation(degrees=5),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomCrop((32, 32), padding=3),       
-            transforms.ToTensor(),
-        ])
-        test_set = CIFAR10(root='./data/cifar10_data', train=False, transform=CIFAR10_transform, download=True)
+        test_set = CIFAR10(root='./data/cifar10_data', train=False, transform=cifar_train_transform, download=True)
         image_shape = (1,3,32,32)
-    elif args.data_name=='cifar100':
+elif args.data_name=='cifar100':
         train_loader, val_loader = cifar100_data_loader(batch_size=args.batch_size)
 
+def KL_divergence( p, q):
+        p = torch.softmax(p, dim=1)
+        q = torch.softmax(q, dim=1)
+        s1 = torch.sum(p * torch.log(p/q))
+        s2 = torch.sum((1-p) * torch.log((1-p)/(1-q)))
+        return s1 + s2
+
+
+def main(): # numclass=0
+    print(args)
 
     model = SparseAutoencoder_all(in_channel=args.data_channel,num_classes=args.num_classes,feature_dim=args.feature_dim, latent_dim=args.latent_dim).to(device) #
     optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)   #
     # optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, weight_decay=1e-4)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50, 100, 150], gamma=0.5)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 210, 270], gamma=args.gamma)
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150, 190], gamma=args.gamma)
     rho = torch.FloatTensor([0.005 for _ in range(args.K)]).unsqueeze(0).to(device) # args.latent_dim
 
     cls = CLoss()
     sls = SparseLoss()
     mse = MSELoss()
-    temp_acc = 0.8
+    temp_acc = 0.93
     # mse = torch.nn.MSELoss()
     for epoch in range(args.epochs):
         train_mse_loss, train_loss, train_correct, train_total = 0, 0, 0, 0
@@ -111,9 +115,11 @@ def main(): # numclass=0
             feat, encoded, decoded = model(inputs)
 
             MSE_loss = args.mse_weight * mse(feat,decoded,targets)
-            C_loss = args.cls_weight * cls(feat, decoded, targets, args.temp)
+            C_loss = args.cls_weight * cls(feat, decoded, targets, args.temp, args.l1)
             if args.use_sparse:
                 Sparse_loss = args.kl_weight * sls(rho, encoded, targets, args.K)
+                # rho_hat, _ = feat.topk(args.K, dim=1,largest=False)
+                # Sparse_loss = KL_divergence(rho, rho_hat)
                 loss = C_loss + Sparse_loss
             else: 
                 loss = C_loss
@@ -147,9 +153,11 @@ def main(): # numclass=0
                 feat, encoded, decoded = model(inputs)
 
                 MSE_loss = args.mse_weight * mse(feat,decoded,targets)
-                C_loss = args.cls_weight * cls(feat, decoded, targets, args.temp)
+                C_loss = args.cls_weight * cls(feat, decoded, targets, args.temp, args.l1)
                 if args.use_sparse:
                     Sparse_loss = args.kl_weight * sls(rho, encoded, targets, args.K)
+                    # rho_hat, _ = feat.topk(args.K, dim=1,largest=False)
+                    # Sparse_loss = KL_divergence(rho, rho_hat)
                     loss = C_loss + Sparse_loss
                 else: 
                     loss = C_loss
@@ -166,11 +174,11 @@ def main(): # numclass=0
             val_acc = val_correct / val_total
             val_mse_loss = val_mse_loss / len(val_loader)  # / 938 = 60032 / 64
 
-        if (epoch % 10 == 0) or (val_acc >= temp_acc): # (val_acc >= temp_acc): val_loss < temp_loss)
+        if (val_acc >= temp_acc): # (val_acc >= temp_acc): val_loss < temp_loss)
             # temp_loss = val_loss
             temp_acc = val_acc
-            torch.save(model.state_dict(), 'saved_models/%s/scpn_%s_ld_%d_ep_%d_va_%.4f.pth'
-                       % (args.data_name, args.data_name, args.latent_dim, epoch, val_acc))
+            torch.save(model.state_dict(), 'saved_models/%s/scpn_%s_ld_%d_ep_%d_l1_%d_s_%d_%.4f.pth'
+                       % (args.data_name, args.data_name, args.latent_dim, epoch, args.l1, args.use_sparse, val_acc))
 
         print(
             'Epoch : %03d  Train Loss: %.3f | Train Acc: %.3f%% | Val Loss: %.3f | Val Acc: %.3f%%'
@@ -212,7 +220,7 @@ def main(): # numclass=0
         plt.gray()
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
-    plt.savefig('AE_Sparse_%d.png'%(args.use_sparse), bbox_inches='tight', pad_inches=.25)
+    plt.savefig('AE_Sparse_%s_%d.png'%(args.data_name, args.use_sparse), bbox_inches='tight', pad_inches=.25)
     plt.show()
 
 
